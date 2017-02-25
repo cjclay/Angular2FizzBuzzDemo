502ac62e62b596475fd7f2bf0f474a4474e20e7f
Ari Frankel <ari.l.frankel@gmail.com>
2017-02-25 16:48:23 -0600

    start writing tests.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,16 +19,25 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'FizzBuzz'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('FizzBuzz');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in an h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('FizzBuzz');
   }));
+
+  it('should render numbers on the page in divs', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('div').toExist())
+  })
+    );
+
 });
