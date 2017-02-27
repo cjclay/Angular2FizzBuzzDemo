644bfa53c469e6d8f533dd5e166008709647f494
Ari Frankel <ari.l.frankel@gmail.com>
2017-02-27 13:09:19 -0600

    solution with *ngFor and Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'FizzBuzz';
   numbers = Array.apply(null, Array(100)).map( (_, i) => i).slice(1);
+  fizzbuzz = function(number){
+    return number % 3 === 0 && number % 5 === 0 ? 'Fizzbuzz'
+    : number % 3 === 0 && number % 5 !== 0 ? 'Fizz'
+    : number % 5 === 0 && number % 3 !== 0 ? 'Buzz'
+    : number
+  }
 }
